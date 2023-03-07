1411b9edcfa05c981c8ff2c3b186325597576b1f
charlotterepper <charlotte.repper01@gmail.com>
2023-03-07 16:03:04 +0100

    Initialize Spring Application

---
+++ backend/src/main/java/com/charlotte/inknote/InkNoteApplication.java
@@ -0,0 +1,13 @@
+package com.charlotte.inknote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InkNoteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InkNoteApplication.class, args);
+	}
+
+}
