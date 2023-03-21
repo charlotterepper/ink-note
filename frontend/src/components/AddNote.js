cdcd87e0679e3c8ce6323f7c21fcf4cefdb72811
charlotterepper <charlotte.repper01@gmail.com>
2023-03-21 15:03:51 +0100

    Create submitNote

--- frontend/src/components/AddNote.js
+++ frontend/src/components/AddNote.js
@@ -3,14 +3,35 @@ import Form from "react-bootstrap/Form";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Button from "react-bootstrap/Button";
+import {useState} from "react";
 
 export default function AddNote() {
+    const [data, setData] = useState({title: "", description: ""});
+
+    async function submitNote(e) {
+        e.preventDefault();
+        try {
+            let result = await fetch("http://localhost:8080/notes/add", {
+                method: "POST",
+                body: JSON.stringify(data),
+                header: {"Content-Type": "application/json"}
+            })
+            if (result.status === 200) {
+                alert("Your note has been created successfully!");
+            } else {
+                alert("An error has occurred: " + result.status);
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
     return (
         <>
             <NavBar/>
             <Container>
                 <Row style={{marginTop: "50px"}} xs lg="2">
-                    <Form>
+                    <Form onSubmit={submitNote}>
                         <Form.Group className="mb-3" controlId="title">
                             <Form.Label>Title</Form.Label>
                             <Form.Control type="text"/>
@@ -19,7 +40,7 @@ export default function AddNote() {
                             <Form.Label>Description</Form.Label>
                             <Form.Control as="textarea" rows={3}/>
                         </Form.Group>
-                        <Button>Create Note</Button>
+                        <Button type="submit">Create Note</Button>
                     </Form>
                 </Row>
             </Container>
