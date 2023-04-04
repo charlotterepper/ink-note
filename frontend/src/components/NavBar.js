d9e6645cd29c9e6c44df034ec1822d2e040d13a3
charlotterepper <charlotte.repper01@gmail.com>
2023-04-04 18:38:28 +0200

    Implement user logout

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -3,6 +3,13 @@ import Navbar from "react-bootstrap/Navbar";
 import Container from "react-bootstrap/Container";
 
 export default function NavBar () {
+    function handleLogout() {
+        localStorage.removeItem("token");
+        localStorage.removeItem("principal");
+        alert("You have logged-out successfully!")
+        window.location.reload();
+    }
+
     return(
       <>
           <Navbar bg="light" variant="light">
@@ -12,7 +19,11 @@ export default function NavBar () {
                       {
                           !localStorage.getItem("token")
                           ? <Nav.Link href="/registration">Sign up</Nav.Link>
-                          : <><Nav.Link href="/notes/all">All Notes</Nav.Link><Nav.Link href="/notes/add">Add Note</Nav.Link></>
+                          : <>
+                                  <Nav.Link href="/notes/all">All Notes</Nav.Link>
+                                  <Nav.Link href="/notes/add">Add Note</Nav.Link>
+                                  <Nav.Link onClick={handleLogout}>Logout</Nav.Link>
+                            </>
                       }
                   </Nav>
               </Container>
