23b9db4d460d1ef5dd5cc88f98143c717ab4bb18
charlotterepper <charlotte.repper01@gmail.com>
2023-04-05 10:08:28 +0200

    Create User Entity

---
+++ backend/src/main/java/com/charlotte/inknote/model/User.java
@@ -0,0 +1,27 @@
+package com.charlotte.inknote.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@RequiredArgsConstructor
+@Getter
+@Setter
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @NonNull
+    private String firstName;
+    @NonNull
+    private String lastName;
+    @NonNull
+    private String email;
+    @NonNull
+    private String password;
+}
