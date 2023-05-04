a9b18f284fde8f695e3e281f5f7e18abf49615b0
charlotterepper <charlotte.repper01@gmail.com>
2023-05-04 18:11:39 +0200

    Remove unused imports

--- backend/src/test/java/com/charlotte/inknote/UserControllerTest.java
+++ backend/src/test/java/com/charlotte/inknote/UserControllerTest.java
@@ -1,13 +1,7 @@
 package com.charlotte.inknote;
 
-import com.charlotte.inknote.controller.NoteController;
 import com.charlotte.inknote.controller.UserController;
-import com.charlotte.inknote.dto.NoteDTO;
-import com.charlotte.inknote.dto.UserDTO;
-import com.charlotte.inknote.model.User;
-import com.charlotte.inknote.service.NoteService;
 import com.charlotte.inknote.service.UserService;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -17,14 +11,10 @@ import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors;
 import org.springframework.test.web.servlet.MockMvc;
 
-import java.util.List;
-
-import static org.hamcrest.Matchers.hasSize;
-import static org.hamcrest.Matchers.is;
-import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(controllers = UserController.class)
 public class UserControllerTest {
