3beb9e8db03c630612f071b0f6291b70c9d61a87
charlotterepper <charlotte.repper01@gmail.com>
2023-05-04 11:23:51 +0200

    Refactor NoteServiceTest

--- backend/src/test/java/com/charlotte/inknote/NoteServiceTest.java
+++ backend/src/test/java/com/charlotte/inknote/NoteServiceTest.java
@@ -1,8 +1,10 @@
 package com.charlotte.inknote;
 
 import com.charlotte.inknote.dto.NoteDTO;
+import com.charlotte.inknote.dto.UserDTO;
 import com.charlotte.inknote.mapper.NoteDTOMapper;
 import com.charlotte.inknote.model.Note;
+import com.charlotte.inknote.model.User;
 import com.charlotte.inknote.repository.NoteRepository;
 import com.charlotte.inknote.service.NoteService;
 import org.junit.jupiter.api.Test;
@@ -34,8 +36,8 @@ public class NoteServiceTest {
 
     @Test
     void testAddNote() {
-        NoteDTO expected = new NoteDTO(1L, "hello", "world");
-        Note note = new Note(1L, "hello", "world");
+        NoteDTO expected = new NoteDTO(1L, "hello", "world", new UserDTO("user@mail.com"));
+        Note note = new Note(1L, "hello", "world", new User());
         Note noteNoId = new Note("hello", "world");
         when(noteRepository.save(any())).thenReturn(note);
         when(noteDTOMapper.toNote(expected)).thenReturn(noteNoId);
@@ -45,12 +47,13 @@ public class NoteServiceTest {
 
         assertEquals(expected.getTitle(), actual.getTitle());
         assertEquals(expected.getDescription(), actual.getDescription());
+        assertEquals(expected.getUserDTO().getEmail(), actual.getUserDTO().getEmail());
     }
 
     @Test
     void testFindAllNotes() {
-        List<Note> notes = List.of(new Note(1L, "hello", "world"));
-        List<NoteDTO> expected = List.of(new NoteDTO(1L, "hello", "world"));
+        List<Note> notes = List.of(new Note(1L, "hello", "world", new User()));
+        List<NoteDTO> expected = List.of(new NoteDTO(1L, "hello", "world", new UserDTO("user@mail.com")));
         when(noteRepository.findAll()).thenReturn(notes);
         when(noteDTOMapper.toNoteDTOList(any())).thenReturn(expected);
 
@@ -58,12 +61,13 @@ public class NoteServiceTest {
 
         assertEquals(expected.get(0).getTitle(), actual.get(0).getTitle());
         assertEquals(expected.get(0).getDescription(), actual.get(0).getDescription());
+        assertEquals(expected.get(0).getUserDTO().getEmail(), actual.get(0).getUserDTO().getEmail());
     }
 
     @Test
     void testUpdateNote() {
-        Note note = new Note(1L, "old title", "old description");
-        NoteDTO noteDTO = new NoteDTO(1L, "new title", "new description");
+        Note note = new Note(1L, "old title", "old description", new User());
+        NoteDTO noteDTO = new NoteDTO(1L, "new title", "new description", new UserDTO("user@mail.com"));
         when(noteRepository.save(any())).thenReturn(note);
         when(noteDTOMapper.toNote(noteDTO)).thenReturn(note);
         when(noteDTOMapper.toNoteDTO(note)).thenReturn(noteDTO);
@@ -72,5 +76,6 @@ public class NoteServiceTest {
 
         assertEquals(noteDTO.getTitle(), actual.getTitle());
         assertEquals(noteDTO.getDescription(), actual.getDescription());
+        assertEquals(noteDTO.getUserDTO().getEmail(), actual.getUserDTO().getEmail());
     }
 }
